37b1b37d0d587a2a9791142430520975acf05479
Dikshit Kumar <dikshitbcrec@gmail.com>
2026-01-29 09:19:43 +0530

    added link

--- src/utility/listOfProject.js
+++ src/utility/listOfProject.js
@@ -2,18 +2,18 @@ export const projectData = [
     {
       title: "Shorten Url",
       description: "Built a full-stack URL Shortener using Spring Boot & MongoDB, featuring Base62 encoding, TTL-based expiry, and atomic counters.",
-      link: "https://myportfolio.com"
+      link: "https://github.com/dikshitbcrec/Shorten-url"
     },
     {
       title: "Birthday Notification",
       description: "Developed a full-stack birthday notification system with backend APIs and a React-based admin dashboard for user and notification management.",
     //   tech: ["MongoDB", "Express", "React", "Node.js"],
-      link: "https://mystore.com"
+      link: "https://github.com/dikshitbcrec/Birthday-Notification"
     },
     {
       title: "Food Ordering Platform",
       description: "Developed a food Ordering platform enabling admins to manage menus, delivery locations, and order status, with Swagger API access and service/cancellation windows for efficient operations.",
     //   tech: ["React", "Node.js", "Socket.io"],
-      link: "https://mychatapp.com"
+      link: "https://github.com/dikshitbcrec/Restaurant-Restful-Api/tree/master/Thunder"
     }
   ];
